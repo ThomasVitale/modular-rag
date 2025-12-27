63e05af47b4f9cc150b8d91d10dc4749cba36fd6
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-12-27 16:43:40 +0100

    Update to Spring Boot 4, Vaadin 25, Arconia 0.21
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- src/main/java/com/thomasvitale/demo/ModularRagApplication.java
+++ src/main/java/com/thomasvitale/demo/ModularRagApplication.java
@@ -1,14 +1,20 @@
 package com.thomasvitale.demo;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.aura.Aura;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @PWA(name = "Modular RAG", shortName = "Modular RAG")
 @Push
+@StyleSheet(Aura.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet("styles.css")
 public class ModularRagApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
